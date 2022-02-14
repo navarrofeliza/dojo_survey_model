574adece8971c5d46b4dd43e45f5e14465ba8fab
navarrofeliza <navarrofeliza@yahoo.com>
2022-02-14 15:10:47 -0800

    dojo survey with models submit

---
+++ DojoSurveyModel/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyModel.Models;
+
+namespace DojoSurveyModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost("process")]
+        public IActionResult ProcessForm(Survey result)
+        {
+
+            // passing result as view model
+            return View("Result", result);
+        }
+    }
+}
